73adea80b6a35a72508a355b1eb8542b6a993823
Fangrui Qin <you@example.com>
2022-04-23 22:56:11 -0400

    math.hypot test

--- tests/float/math_fun_special.py
+++ tests/float/math_fun_special.py
@@ -51,28 +51,19 @@ for function_name, function, test_vals in functions:
         except ValueError as e:
             print(str(e))
 
-
-# Tests for hypot
-# https://github.com/python/cpython/blob/main/Lib/test/test_math.py
-
-
-def assertRaises(exception, func, *args):
-    try:
-        print(func(*args))
-        assert False
-    except exception:
-        pass
-
-# Test allowable types (those with __float__)
-print(hypot(12.0, 5.0))
-print(hypot(12, 5), 13)
-print(hypot(Decimal(12), Decimal(5)))
-print(hypot(Fraction(12, 32), Fraction(5, 32)))
-print(hypot(bool(1), bool(0), bool(1), bool(1)))
-
-# Test corner cases
-print(hypot(0.0, 0.0))     # Max input is zero
-print(hypot(-10.5))       # Negative input
-print(hypot())             # Negative input
-print(hypot(1.5, 1.5, 0.5), hypot(1.5, 0.5, 1.5),)# Handling of moving max to the end
-
+hypot_func_var_args = [
+    (
+        "hypot",
+        hypot,
+        (
+            (12.0, 5.0),
+            (12, 5),
+            (bool(1), bool(0), bool(1), bool(1)),
+            (0.0, 0.0),
+            (-10.5),
+            (),
+            (1.5, 1.5, 0.5),
+            (1.5, 0.5, 1.5),
+        ),
+    ),
+]
